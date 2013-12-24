1cd6d8ba81f04cde110d95ba381bcb0891a5b1ba
Michael Turnwall <michael.turnwall@digitaria.com>
2013-12-24 02:06:46 -0800

    Initial commit

---
+++ template.js
@@ -0,0 +1,25 @@
+/**
+ *  grunt-init-digitaria
+ *
+ *	Project scaffolding using grunt.js for Digitaria projects
+ *  
+ *  Copyright (c) 2013 Digitaria, Inc.
+ *  Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Project scaffolding using grunt.js for Digitaria projects.';
+
+// Template-specific notes to be displayed before question prompts.
+// exports.notes = '';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '';
+
+// The actual init template
+exports.template = function(grunt, init, done) {
+  // See the "Inside an init template" section.
+};
+
