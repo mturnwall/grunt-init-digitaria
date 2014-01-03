26e925c6e50fb32f031966414c949f59a4521c22
Michael Turnwall <michael.turnwall@digitaria.com>
2014-01-02 17:09:49 -0800

    Update grunt template and default files
    The template now works and outputs a package.json file. Default files are copied and dynamically renamed.

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 /**
  *  grunt-init-digitaria
  *
- *	Project scaffolding using grunt.js for Digitaria projects
+ *  Project scaffolding using grunt.js for Digitaria projects
  *  
  *  Copyright (c) 2013 Digitaria, Inc.
  *  Licensed under the MIT license.
@@ -20,6 +20,70 @@ exports.warnOn = '';
 
 // The actual init template
 exports.template = function(grunt, init, done) {
-  // See the "Inside an init template" section.
+    
+    init.process({}, [
+        init.prompt('name'),
+        init.prompt('title'),
+        init.prompt('description'),
+        init.prompt('version'),
+        init.prompt('repository', ''),
+        init.prompt('jquery_version'),
+        {
+            name: 'config_rb_path',
+            message: 'Path to the config.rb file',
+            warning: 'If it\'s drupal the path will be /sites/all/themes/project-name'
+        }
+        {
+            name: 'handlebars',
+            message: 'Are handlebars templates going to be used?',
+            default: 'y/N'
+        },
+        {
+            name: 'unitTests',
+            message: 'Will there be javascript unit testing?',
+            default: 'y/N'
+        }
+
+    ], function (err, props) {
+        var files;
+
+        props.handlebars = /y/i.test(props.handlebars);
+        props.unitTests = /y/i.test(props.unitTests);
+
+        props.author_name = 'Digitaria, Inc';
+        props.author_url = 'http://www.digitaria.com';
+        props.devDependencies = {
+            "grunt": "~0.4.2",
+            "grunt-contrib-concat": "~0.3.0",
+            "grunt-contrib-jshint": "~0.7.2",
+            "grunt-contrib-watch": "~0.5.3",
+            "grunt-contrib-compass": "~0.7.0",
+            "grunt-contrib-clean": "~0.5.0",
+            "grunt-contrib-copy": "~0.4.1",
+            "matchdep": "~0.3.0",
+            "grunt-contrib-imagemin": "~0.4.0"
+        };
+
+        // Files to copy (and process).
+        files = init.filesToCopy(props);
+
+        // Actually copy (and process) files.
+        init.copyAndProcess(files, props)
+
+        if (props.handlebars) {
+            props.devDependencies['grunt-contrib-handlebars'] = '~0.6.0';
+        }
+
+        if (props.unitTests) {
+            /*
+                TODO
+                Find a good grunt plugin for javascript unit tests (mocha+chai)
+             */
+        }        
+
+        init.writePackageJSON('package.json', props);
+
+        done();
+    });
 };
 
